7b8c796397ba82cff5b5142dfe7d9a6cdcb14b7a
ehu9480 <86241687+ehu9480@users.noreply.github.com>
2024-02-14 03:09:14 -0500

    switching to gremlins channel id

--- bot.py
+++ bot.py
@@ -9,8 +9,8 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-#CHANNEL_ID = 1071187673676529876
-CHANNEL_ID = 1204200927536488508 #TEST
+CHANNEL_ID = 1071187673676529876
+#CHANNEL_ID = 1204200927536488508 #TEST
 
 bot = commands.Bot(command_prefix="!", intents = discord.Intents.all())
 scheduler = AsyncIOScheduler()
